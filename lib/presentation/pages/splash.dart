9a2ee131671234f637ab201bf04ff024cfa21ef9
jishnukp2482 <jishnuvidyadharan123@gmail.com>
2024-10-11 10:21:42 +0530

    ebs bill done

--- lib/presentation/pages/splash.dart
+++ lib/presentation/pages/splash.dart
@@ -167,55 +167,34 @@ class _SplashScreenState extends State<SplashScreen>
 class StarPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    // Layer 1
 
     Paint paintFill0 = Paint()
-      ..color = const Color.fromARGB(255, 255, 255, 255)
+      ..color = AppColors.backgroundcolor
       ..style = PaintingStyle.fill
       ..strokeWidth = size.width * 0.00
       ..strokeCap = StrokeCap.butt
       ..strokeJoin = StrokeJoin.miter;
 
     Path path_0 = Path();
-    path_0.moveTo(size.width * 0.4942083, size.height * -1.0906857);
-    path_0.lineTo(size.width * 0.5045083, size.height * -0.7990286);
-    path_0.lineTo(size.width * 0.5240917, size.height * -0.5506286);
-    path_0.lineTo(size.width * 0.5815917, size.height * -0.2862429);
-    path_0.lineTo(size.width * 0.6454583, size.height * -0.0166714);
-    path_0.lineTo(size.width * 0.7974250, size.height * 0.2414857);
-    path_0.lineTo(size.width * 0.9740500, size.height * 0.2561714);
-    path_0.lineTo(size.width * 1.1411083, size.height * 0.2547000);
-    path_0.lineTo(size.width * 1.2924750, size.height * 0.2751000);
-    path_0.lineTo(size.width * 1.4342583, size.height * 0.2522000);
-    path_0.lineTo(size.width * 1.2831917, size.height * 0.3400000);
-    path_0.lineTo(size.width * 1.1320000, size.height * 0.3791143);
-    path_0.lineTo(size.width * 0.9746250, size.height * 0.4453571);
-    path_0.lineTo(size.width * 0.8172167, size.height * 0.5438286);
-    path_0.lineTo(size.width * 0.6542083, size.height * 0.8101714);
-    path_0.lineTo(size.width * 0.5918000, size.height * 1.0593000);
-    path_0.lineTo(size.width * 0.5327167, size.height * 1.3243857);
-    path_0.lineTo(size.width * 0.5050417, size.height * 1.5949571);
-    path_0.lineTo(size.width * 0.5027500, size.height * 1.8758714);
-    path_0.lineTo(size.width * 0.4735833, size.height * 1.5952143);
-    path_0.lineTo(size.width * 0.3751417, size.height * 1.3366429);
-    path_0.lineTo(size.width * 0.3617000, size.height * 1.0773571);
-    path_0.lineTo(size.width * 0.3609750, size.height * 0.8071429);
-    path_0.lineTo(size.width * 0.1899583, size.height * 0.5384714);
-    path_0.lineTo(size.width * 0.0102833, size.height * 0.5344714);
-    path_0.lineTo(size.width * -0.1377750, size.height * 0.5574143);
-    path_0.lineTo(size.width * -0.3049667, size.height * 0.5209857);
-    path_0.lineTo(size.width * -0.4563083, size.height * 0.5222286);
-    path_0.lineTo(size.width * -0.3022083, size.height * 0.3859714);
-    path_0.lineTo(size.width * -0.1354333, size.height * 0.2925143);
-    path_0.lineTo(size.width * 0.0315500, size.height * 0.2749143);
-    path_0.lineTo(size.width * 0.1733667, size.height * 0.2414857);
-    path_0.lineTo(size.width * 0.3018083, size.height * -0.0137571);
-    path_0.lineTo(size.width * 0.3388917, size.height * -0.2787429);
-    path_0.lineTo(size.width * 0.4295083, size.height * -0.5606286);
-    path_0.lineTo(size.width * 0.4634667, size.height * -0.8095143);
-    path_0.lineTo(size.width * 0.4942083, size.height * -1.0906857);
+    path_0.moveTo(size.width * 0.6863682, size.height * -0.9557429);
+    path_0.lineTo(size.width * 0.4240078, size.height * -0.2883000);
+    path_0.lineTo(size.width * 0.1767968, size.height * 0.0555571);
+    path_0.lineTo(size.width * -0.3227677, size.height * 0.3926714);
+    path_0.lineTo(size.width * 0.1818109, size.height * 0.7163143);
+    path_0.lineTo(size.width * 0.4341110, size.height * 1.0601429);
+    path_0.lineTo(size.width * 0.6813968, size.height * 1.7411429);
+    path_0.lineTo(size.width * 0.9286292, size.height * 1.0668714);
+    path_0.lineTo(size.width * 1.1909789, size.height * 0.7298286);
+    path_0.lineTo(size.width * 1.6905007, size.height * 0.3859000);
+    path_0.lineTo(size.width * 1.1858899, size.height * 0.0689714);
+    path_0.lineTo(size.width * 0.9386682, size.height * -0.2883000);
+    path_0.lineTo(size.width * 0.6863682, size.height * -0.9557429);
     path_0.close();
 
     canvas.drawPath(path_0, paintFill0);
+
+    // Layer 1
 
     Paint paintStroke0 = Paint()
       ..color = const Color.fromARGB(255, 33, 150, 243)
